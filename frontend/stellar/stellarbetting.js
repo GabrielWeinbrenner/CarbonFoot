b0bdcf6c22ac4049122c625c7f3b351d72df98c5
Ryan Lee <ryanlee7132@gmail.com>
2020-03-07 20:11:22 -0500

    Update stellarbetting.js

--- frontend/stellar/stellarbetting.js
+++ frontend/stellar/stellarbetting.js
@@ -1,16 +1,18 @@
+// Parameters: Destination (account ID), Asset (default USD), Amount (integer)
+
 const StellarSdk = require('stellar-sdk');
 const sourceSecretKey = 'SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4';
 const sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
 const sourcePublicKey = sourceKeypair.publicKey();
 const receiverPublicKey = 'GAIRISXKPLOWZBMFRPU5XRGUUX3VMA3ZEWKBM5MSNRU3CHV6P4PYZ74D';
-const server = new StellarSdk.Server('https://horizon.stellar.org');
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
 (async function main() {
   const account = await server.loadAccount(sourcePublicKey);
   const fee = await server.fetchBaseFee();
   const transaction = new StellarSdk.TransactionBuilder(account, { 
       fee,
-      networkPassphrase: StellarSdk.Networks.PUBLIC
+      networkPassphrase: StellarSdk.Networks.TESTNET
     })
     .addOperation(StellarSdk.Operation.payment({
       destination: receiverPublicKey,
