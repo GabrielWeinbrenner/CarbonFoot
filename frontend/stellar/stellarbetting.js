27f2c932e98b4fc7c1188e9d8516de596ba709a4
Ryan Lee <ryanlee7132@gmail.com>
2020-03-07 20:36:02 -0500

    Update stellarbetting.js

--- frontend/stellar/stellarbetting.js
+++ frontend/stellar/stellarbetting.js
@@ -1,4 +1,4 @@
-// Parameters: Destination (account ID), Asset (only can be lumens (XLM)), Amount (integer)
+// Parameters: Destination (account ID), Asset (only can be lumens (XLM)), Amount of lumens (integer)
 
 const StellarSdk = require('stellar-sdk');
 const sourceSecretKey = 'SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4';
@@ -6,6 +6,13 @@ const sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
 const sourcePublicKey = sourceKeypair.publicKey();
 const receiverPublicKey = 'GAIRISXKPLOWZBMFRPU5XRGUUX3VMA3ZEWKBM5MSNRU3CHV6P4PYZ74D';
 const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+
+const pair = StellarSdk.Keypair.random();
+
+pair.secret();
+// SAV76USXIJOBMEQXPANUOQM6F5LIOTLPDIDVRJBFFE2MDJXG24TAPUU7
+pair.publicKey();
+// GCFXHS4GXL6BVUCXBWXGTITROWLVYXQKQLF4YH5O5JT3YZXCYPAFBJZB
 
 (async function main() {
   const account = await server.loadAccount(sourcePublicKey);
