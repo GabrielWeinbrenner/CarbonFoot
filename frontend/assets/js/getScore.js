54126c9f2de0de2e3e41cc6e6957e17f520cd41d
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-03-08 11:12:15 -0400

    Leaderboard rendering

--- frontend/assets/js/getScore.js
+++ frontend/assets/js/getScore.js
@@ -2,12 +2,14 @@ var length;
 var currentScore;
 var advice;
 var companies;
+var scoreboard;
 $.when($.getJSON("http://localhost:3001/carbon")).then(function(data){
     length = data.length;
     $.when($.get("http:localhost:3001/carbon/"+length)).then(function(data){
         currentScore = data.score
         advice = data.advice;
         companies = data.companies;
+        
         console.log(data);
         if(currentScore < 6000){
             $("#score").html("<h2 style='color:green;'>Score: "+currentScore +"lbs of CO2</h2>")
@@ -25,4 +27,14 @@ $.when($.getJSON("http://localhost:3001/carbon")).then(function(data){
     })
 
 })
-
+$.when($.get("http:localhost:3001/carbon")).then(function(data){
+    for(var i =0; i< data.length; i++){
+        console.log(data[i].name);
+        $("#leaders").append(
+            "<tr><td>"+data[i].name+"</td><td>"+
+            data[i].score+"</td><td>"+
+			"<img id='challenge'src='images/challenge.png' style='position:center; height:30px;'/>"+
+            "</tr>"
+        )
+    }    
+})
