9820e6c04b3e4e869de82174a09d3e0e1a50d12b
Ryan Lee <ryanlee7132@gmail.com>
2020-03-07 21:05:48 -0500

    Update stellarbetting.js

--- frontend/stellar/stellarbetting.js
+++ frontend/stellar/stellarbetting.js
@@ -1,14 +1,18 @@
-// Parameters: Source account (your ID), Destination (their account ID), Asset (only can be lumens (XLM)), Amount of lumens (integer)
+// Parameters: Source account (your ID), vDestination (their account ID), Asset (only can be lumens (XLM)), Amount of lumens (integer)
 // Can use testnet server for demos
 
-const StellarSdk = require('stellar-sdk');
-const sourceSecretKey = 'SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4';
-const sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
-const sourcePublicKey = sourceKeypair.publicKey();
-const receiverPublicKey = 'GAIRISXKPLOWZBMFRPU5XRGUUX3VMA3ZEWKBM5MSNRU3CHV6P4PYZ74D';
-const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+var StellarSdk = require('stellar-sdk');
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+var sourceKeys = StellarSdk.Keypair.fromSecret('SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4');
+var destinationId = 'GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5';
+var transaction;
+var accountId = 'GC2BKLYOOYPDEFJKLKY6FNNRQMGFLVHJKQRGNSSRRGSMPGF32LHCQVGF';
+var payments = server.payments().forAccount(accountId);
+var lastToken = loadLastPagingToken();
 
 const pair = StellarSdk.Keypair.random();
+const account = await server.loadAccount(pair.publicKey());
+
 
 pair.secret();
 // SAV76USXIJOBMEQXPANUOQM6F5LIOTLPDIDVRJBFFE2MDJXG24TAPUU7
@@ -16,33 +20,84 @@ pair.publicKey();
 // GCFXHS4GXL6BVUCXBWXGTITROWLVYXQKQLF4YH5O5JT3YZXCYPAFBJZB
 
 (async function main() {
-  const account = await server.loadAccount(sourcePublicKey);
-  const fee = await server.fetchBaseFee();
-  const transaction = new StellarSdk.TransactionBuilder(account, { 
-      fee,
+  try {
+    const response = await fetch(
+      `https://friendbot.stellar.org?addr=${encodeURIComponent(pair.publicKey())}`
+    );
+    const responseJSON = await response.json();
+    console.log("SUCCESS! You have a new account :)\n", responseJSON);
+  } catch (e) {
+    console.error("ERROR!", e);
+  }
+})()
+
+console.log("Balances for account: " + pair.publicKey());
+account.balances.forEach(function(balance) {
+  console.log("Type:", balance.asset_type, ", Balance:", balance.balance);
+});
+
+server.loadAccount(destinationId)
+  .catch(function (error) {
+    if (error instanceof StellarSdk.NotFoundError) {
+      throw new Error('The destination account does not exist!');
+    } else return error
+  })
+  .then(function() {
+    return server.loadAccount(sourceKeys.publicKey());
+  })
+  .then(function(sourceAccount) {
+    transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
+      fee: StellarSdk.BASE_FEE,
       networkPassphrase: StellarSdk.Networks.TESTNET
     })
-    .addOperation(StellarSdk.Operation.payment({
-      destination: receiverPublicKey,
-      // The term native asset refers to lumens
-      asset: StellarSdk.Asset.native(),
-      amount: '350.1234567',
-    }))
-    .setTimeout(30)
-    .addMemo(StellarSdk.Memo.text('This is a Carbon Footprint challenge!'))
-    .build();
+      .addOperation(StellarSdk.Operation.payment({
+        destination: destinationId,
+        asset: StellarSdk.Asset.native(),
+        amount: "10"
+      }))
+      .addMemo(StellarSdk.Memo.text('Test Transaction'))
+      .setTimeout(180)
+      .build();
+    transaction.sign(sourceKeys);
+    return server.submitTransaction(transaction);
+  })
+  .then(function(result) {
+    console.log('Success! Results:', result);
+  })
+  .catch(function(error) {
+    console.error('Something went wrong!', error);
+  });
+  
+if (lastToken) {
+  payments.cursor(lastToken);
+}
 
-  transaction.sign(sourceKeypair);
+payments.stream({
+  onmessage: function(payment) {
+    savePagingToken(payment.paging_token);
 
-  console.log(transaction.toEnvelope().toXDR('base64'));
+    if (payment.to !== accountId) {
+      return;
+    }
 
-  try {
-    const transactionResult = await server.submitTransaction(transaction);
-    console.log(JSON.stringify(transactionResult, null, 2));
-    console.log('\nSuccess! View the transaction at: ');
-    console.log(transactionResult._links.transaction.href);
-  } catch (e) {
-    console.log('An error has occured:');
-    console.log(e);
+    var asset;
+    if (payment.asset_type === 'native') {
+      asset = 'lumens';
+    }
+    else {
+      asset = payment.asset_code + ':' + payment.asset_issuer;
+    }
+
+    console.log(payment.amount + ' ' + asset + ' from ' + payment.from);
+  },
+
+  onerror: function(error) {
+    console.error('Error in payment stream');
   }
-})();
+});
+
+function savePagingToken(token) {
+}
+
+function loadLastPagingToken() {
+}
