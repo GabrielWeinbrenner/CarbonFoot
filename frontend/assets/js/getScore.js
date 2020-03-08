180cce9c2f812f3acabf50e7c73f2db1f914fab0
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-03-08 03:00:40 -0400

    Question creation

--- frontend/assets/js/getScore.js
+++ frontend/assets/js/getScore.js
@@ -4,8 +4,14 @@ $.when($.getJSON("http://localhost:3001/carbon")).then(function(data){
     length = data.length;
     console.log(length)
     $.when($.get("http:localhost:3001/carbon/"+length)).then(function(data){
-        currentScore =data.score
-        $("#score").html("<h2>Score: "+currentScore +"</h2>")
+        currentScore = data.score
+        if(currentScore < 6000){
+            $("#score").html("<h2 style='color:green;'>Score: "+currentScore +"lbs of CO2</h2>")
+        }else if(currentScore > 6000 && currentScore < 7500){
+            $("#score").html("<h2 style='color:#9B870C;'>Score: "+currentScore +"lbs of CO2</h2>")
+        }else if(currentScore > 7500){
+            $("#score").html("<h2 style='color:red;'>Score: "+currentScore +"lbs of CO2</h2>")
+        }
 
     })
 
--- frontend/assets/js/posting.js
+++ frontend/assets/js/posting.js
@@ -3,58 +3,69 @@
 // # 200 miles for work transportation per week
 // # https://www.nrc.gov/docs/ML1006/ML100621425.pdf
 function getMethodOfTransportation(trans){
+    var co2 = 0;
     if(trans == "electric_car"){
-        return 1066
+        co2 = 1066
     }
     else if(trans == "bus"){
-        return 1216
+        co2 = 1216
     }
     else if(trans == "train"){
-        return 2658
+        co2 = 2658
     }
     else if(trans == "car"){
-        return 4000
+        co2 = 4000
     }
     else{
-        return 0
+        co2 = 0
     }
+    console.log("Transportation: " + co2);
+    return co2
 }
 // # https{//www.conservation.org/carbon-footprint-calculator#/individual?zipCodeInfo=usAverage
 
 function getDiet(diet){
+    var co2 = 0;
     if(diet == "out"){
-        return 2000
+        co2 = 2000
     }
     else if(diet == "home"){
-        return 1500
+        co2 = 1500
     }
-    return 0
+    console.log("Diet: " + co2)
+    return co2
 }
 // # https{//www.conservation.org/carbon-footprint-calculator#/individual?zipCodeInfo=usAverage
 function getMeat(times){
-    if(times == 0){
-        return 2216
+    times = parseInt(times);
+    var co2 = 0;
+    if(times === 0){
+        co2 = 2216
     }else if(times < 5 && times > 3 ){
-        return 2316
+        co2 = 2316
     }
-    else if(times > 6){
-        return 2450
+    else if(times >= 6){
+        co2 = 2450
     }
-    return 0
+    console.log("Beef: " + co2)
+
+    return co2
 }
 
 // # https://inhabitat.com/the-pros-and-cons-of-online-versus-in-store-shopping/
 function getShopping(shoppingType){
+    var co2 = 0;
     if(shoppingType == "online"){
-        return 2000
+        co2 = 2000
     }
     else if(shoppingType == "thrift"){
-        return 1000
+        co2 = 1000
     }
     else if(shoppingType == "instore"){
-        return 1500
+        co2 = 1500
     }
-    return 0
+    console.log("Shopping: " + co2)
+    return co2
 }
 
 // # TOTAL SCORE
@@ -62,12 +73,37 @@ function getTotal(transType, diet, meatAmount,typeShopping){
     return (getMethodOfTransportation(transType) + getDiet(diet) + getMeat(meatAmount) + getShopping(typeShopping));
 }
 
+function generateAdvice(transType, diet, meatAmount,typeShopping){
+    var advice = [];
+    if(transType == "car"){
+        advice.push("Limit your car usage and use public transportation");
+    }
+    if(transType == "bus" || transType == "train"){
+        advice.push("Maybe start walking/biking to your workspace ");
+    }
+    if(diet == "out"){
+        advice.push("Start eating at home to reduce carbon emmssions")
+    }
+    if(meatAmount > 5){
+        advice.push("To reduce carbon emmissions, start limiting your meat consumption")
+    }
+    if(typeShopping == "online"){
+        advice.push("Start going to stores physically or shop at local stores")
+    }
+    if(typeShopping == "instore"){
+        advice.push("Start thrifting your items to reduce CO2 emmissions")
+    }
+    console.log(advice);
+    return advice;
+}
+
 $("#submit").click(function(){
     var transportation = $('input:radio[name=transport]:checked').val();
     var beef = $("#dinner1").val();
     var mealprep = $('input:radio[name=meals]:checked').val();
     var product = $('input:radio[name=shopping]:checked').val();
-    var score = getTotal(transportation, mealprep, beef, product)
+    var score = getTotal(transportation, mealprep, beef, product);
+    var advice = generateAdvice(transportation,mealprep,beef,product);
     $.post("http://localhost:3001/carbon", 
     {
         "name": $("#name").val(),
@@ -75,6 +111,9 @@ $("#submit").click(function(){
         "beef": beef,
         "mealprep": mealprep,
         "product": product,
-        "score": score
-    });
+        "score": score,
+        "advice": advice
+    }).done(function(){
+        window.location.href = "dashboard.html";
+    })
 });
